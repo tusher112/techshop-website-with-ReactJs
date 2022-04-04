548f0288072da7d1cba93866d1a4eccab76b2736
tusher112 <mdtusherahmed820@gmail.com>
2022-04-05 02:10:21 +0600

    see more button added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,7 +5,9 @@ import {
     LineChart,
     Line,
     CartesiaGrid,
-    CartesianGrid
+    CartesianGrid,
+    ComposedChart,
+    Area
 } from 'recharts';
 
 const Dashboard = () => {
@@ -52,28 +54,48 @@ const Dashboard = () => {
 
     return (
 
-        <div>
-            <LineChart width={400} height={500} data={data}>
+        <div className='row '>
 
-                <Line dataKey={"investment"}></Line>
-                <Line dataKey={"sell"}></Line>
-                <Line dataKey={"revenue"}></Line>
-                <XAxis dataKey="month"></XAxis>
-                <YAxis></YAxis>
-                <Tooltip></Tooltip>
-            </LineChart>
+            <div className='col'>
+
+                <LineChart width={400} height={500} data={data}>
+
+                    <Line dataKey={"investment"}></Line>
+                    <Line dataKey={"sell"}></Line>
+                    <Line dataKey={"revenue"}></Line>
+                    <XAxis dataKey="month"></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </div>
 
 
-            <BarChart width={400} height={500} data={data}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="month" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="investment" fill="#8884d8" />
-                <Bar dataKey="revenue" fill="#82ca9d" />
-            </BarChart>
+            <div className="col">
+                <BarChart width={400} height={500} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="investment" fill="#8884d8" />
+                    <Bar dataKey="revenue" fill="#82ca9d" />
+                </BarChart>
+            </div>
 
+
+            <div className="col-12 col-md-6 col-lg-6">
+                <ComposedChart width={630} height={350} data={data}>
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <Area type="monotone" dataKey="investment" fill="#8884d8" stroke="#8884d8" />
+                    <Bar dataKey="sell" barSize={20} fill="#413ea0" />
+                    <Line type="monotone" dataKey="revenue" stroke="#ff7300" />
+                </ComposedChart>
+
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import useReview from '../../hooks/useReview';
 import Homes from '../Homes/Homes';
 import Review from '../Review/Review';
@@ -6,7 +8,7 @@ import './Home.css'
 
 
 const Home = () => {
-            const [review,setReview]=useReview();
+    const [review, setReview] = useReview();
     return (
         <div>
             <div className='home-container'>
@@ -14,7 +16,7 @@ const Home = () => {
                     <h1>Make your Computer 100X Faster!</h1>
                     <p className='para-heading'> The GeForce RTX 3060 is a performance-segment graphics card by NVIDIA, launched on January 12th, 2021. Built on the 8 nm process, and based on the GA106 graphics processor, in its GA106-300-A1 variant, the card supports DirectX 12 Ultimate. This ensures that all modern games will run on GeForce RTX 3060.
                     </p>
-                    <button className='demo-btn'>Live Demo</button>
+                    <Button variant='primary disabled'>Live Demo</Button>
 
                 </div>
 
@@ -27,15 +29,17 @@ const Home = () => {
             <hr />
 
             <div >
-                <h1 >Customer Review</h1>
-                            
-                 {
-                    review.map(reviews => <Homes
+                <h1 >Customer Review (3)</h1>
+
+                {
+                    review.slice(0, 3).map(reviews => <Homes
                         key={reviews.id}
                         reviews={reviews}
                     ></Homes>)
                 }
-            
+
+                <Link to='/review'> <Button variant='success'> See More Reviews</Button> </Link>
+
             </div>
         </div>
     );
