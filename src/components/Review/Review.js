ec90ba300521497576081fee50951d7bf5972b3d
tusher112 <mdtusherahmed820@gmail.com>
2022-04-04 17:14:35 +0600

    reviewState custom hook added

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import useReview from '../../hooks/useReview';
 
 const Review = () => {
+
+    const [review, setReview]=useReview()
     return (
-        <div>
-            <h2>This is review</h2>
+        <div className='container pt-5'>
+            <h2>Reviews ({review.length})</h2>
+
+            <p>Get specific details about this product from customers who own it.</p>
+            <hr />
+            <div>
+
+                
+            </div>
+
         </div>
     );
 };
